2da5e3aade2e70c02fa6bcb8cc096d743c98c042
Md fareed <fareedzaman501@gmail.com>
2024-10-20 19:38:06 +0530

    task func

--- src/taskManager.ts
+++ src/taskManager.ts
@@ -1,6 +1,7 @@
 import { todo } from "node:test";
 import { Task } from "./models/task";
 import { readTasks, writeTasks } from "./utils/fileHandler";
+import { read, stat } from "node:fs";
 
 let currentId = 1;
 function getNextId(): number {
@@ -21,4 +22,37 @@ export function addTask(description: string): void {
   tasks.push(newTask);
   writeTasks(tasks);
   console.log(`Task Added Successfully (ID: ${newTask.id}) `);
+}
+
+export function updateTask(id: number, description: string): void {
+  const tasks = readTasks();
+  const task = tasks.find((task) => task.id === id);
+  if (!task) {
+    console.log(`Task not found !`);
+    return;
+  }
+  task.description = description;
+  task.updatedAt = new Date().toISOString();
+  writeTasks(tasks);
+  console.log("Task updated successfully");
+}
+
+export function deleteTask(id: number): void {
+  let tasks = readTasks();
+  const task = tasks.filter((task) => task.id === id);
+  writeTasks(tasks);
+  console.log("Task deleted successfully.");
+}
+
+export function markTest(id: number, status: "in-progress" | "done"): void {
+  const tasks = readTasks();
+  const task = tasks.find((task) => task.id === id);
+  if (!task) {
+    console.error("Task not find.");
+    return;
+  }
+  task.status = status;
+  task.updatedAt = new Date().toISOString();
+  writeTasks(tasks);
+  console.log(`Task marked as ${status}`);
 }
